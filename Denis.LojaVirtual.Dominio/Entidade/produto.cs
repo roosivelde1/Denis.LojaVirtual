a5af632238c87270e1af47a8496dd41ca0e8651a
roosivelde1 <roosivelde1@live.com>
2015-09-07 15:14:13 -0300

    Class Produto

---
+++ Denis.LojaVirtual.Dominio/Entidade/produto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Denis.LojaVirtual.Dominio.Entidade
+{
+    class produto
+    {
+        public int ProdutoId { get; set; }
+        public string Nome { get; set; }
+        public string Descrição { get; set; }
+        public string Preco { get; set; }
+        public string Ctegoria { get; set; }
+                
+
+
+    }
+}
