e4d4fad407a12570496e55d6dd79957843c8ec4e
roosivelde1 <roosivelde1@live.com>
2015-09-07 14:17:30 -0300

    initial commit

---
+++ Denis.LojaVirtual.UnitTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Denis.LojaVirtual.UnitTest
+{
+    [TestClass]
+    public class UnitTestDenis
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Denis.LojaVirtual.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Denis.LojaVirtual.Web.MvcApplication" Language="C#" %>
